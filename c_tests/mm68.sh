e712916625f59641c0f56d1005fa938702e9093d
davidly <davidly_lee@hotmail.com>
2025-04-14 16:06:56 -0700

    fix typo

--- c_tests/mm68.sh
+++ c_tests/mm68.sh
@@ -25,6 +25,6 @@ $gcccmd $incpaths -mcpu=68000 -x c++ $defines -O2 newlib68.c -S -fverbose-asm -o
 # build the assembly portion with _start and syscalls
 $gccpath/bin/m68k-elf-as -mcpu=68000 m68start.s -o m68start.o
 
-$gcccmd $defines $incpaths -mcpu=68000 -x c++ -O2 ../m68.cxx ../m68000.cxx newlib68.c -l:m68start.o -L./ -static-libgcc -l:libm.a -l:libstdc++.a -static -o $m68.elf
+$gcccmd $defines $incpaths -mcpu=68000 -x c++ -O2 ../m68.cxx ../m68000.cxx newlib68.c -l:m68start.o -L./ -static-libgcc -l:libm.a -l:libstdc++.a -static -o m68.elf
 
 
