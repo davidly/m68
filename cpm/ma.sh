b78805aeddbfc02b114b78f4c19855fb1ffa3ae3
davidly <davidly_lee@hotmail.com>
2025-04-24 16:28:02 -0700

    linux test build fixes

--- cpm/ma.sh
+++ cpm/ma.sh
@@ -3,8 +3,8 @@
 
 str=$(tr '[a-z]' '[A-Z]' <<< $1)
 
-../m68 as68.68k -l -u -s 0: %1.S
-../m68 lo68.68k -r -u_nofloat -o %1.68K 0:S.O %1.O 0:clib
+../m68 as68.68k -l -u -s 0: $str.S
+../m68 lo68.68k -r -u_nofloat -o $str.68K 0:S.O $str.O 0:CLIB
 
 
 
