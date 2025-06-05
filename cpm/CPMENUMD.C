b50486112289033f4614d3a458307574a8aa0de0
davidly <davidly_lee@hotmail.com>
2025-06-05 16:10:33 -0700

    improve the directory enumeration test

--- cpm/CPMENUMD.C
+++ cpm/CPMENUMD.C
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 
-// the C runtime provides __BDOS
+char * malloc();
 
 typedef unsigned char uint8_t;
 typedef char int8_t;
@@ -15,32 +15,45 @@ typedef short int16_t;
 typedef unsigned long uint32_t;
 typedef long int32_t;
 typedef int bool;
-typedef unsigned int size_t;
 
 #define false 0
 #define true 1
-#define INT_MAX 32767
 
-extern uint8_t _start;  // app entry point, 128 bytes above the default DMA address
+#ifdef CPM80
+#define DEFAULT_DMA 128
+#else
+#define bdos __BDOS
+extern uint8_t _start;
+#define DEFAULT_DMA ( ( & _start ) - 128 )
+#endif
 
-struct FCBCPM68K // file control block for cp/m
+struct FCBCPM 
 {
-    uint8_t dr;         // drive
-    char n[ 8 ];        // filename
-    char t[ 3 ];        // file type
-    uint8_t ex;         // current extent number. normally set to 0 by user but is in range 0-31
+    uint8_t dr;      
+    char n[ 8 ];     
+    char t[ 3 ];     
+    uint8_t ex;      
     uint8_t s1;
-    uint8_t s2;         // reserved for system use. set to 0 for open/make/search
-    uint8_t rc;         // record count, reserved for system use
+    uint8_t s2;      
+    uint8_t rc;      
     uint8_t d[ 16 ];
-    uint8_t cr;         // current reacord to be read or written for sequential read/write operations. apps must set approprately.
-    uint8_t r0;         // (optional if app is doing random I/O) random record number. most significant byte in r0 then r1 then r2 as a 24-bit record
-    uint8_t r1;         // the byte order is the opposite of CP/M 2.2 for 8080 plus r2 is actually used!
-    uint8_t r2;
+    uint8_t cr;      
+    uint8_t r0;      
+    uint8_t r1;      
+    uint8_t r2;   /* CP/M 68K and >= CP/M 80 v3 */
 };
 
-struct FCBCPM68K the_fcb;
-char * malloc();
+char * x_strchr( str, c ) char * str; char c;
+{
+    while ( *str )
+    {
+        if ( *str == c )
+            return (char *) str; 
+        str++;
+    }
+
+    return 0;
+}
 
 char * x_memset( p, v, c ) char * p; int v; int c;
 {
@@ -61,6 +74,63 @@ char * x_memcpy( a, b, c ) char * a; char * b; int c;
         c--;
     }
     return dst;
+}
+
+bool fcb_initialize( pfcb, pfilename ) struct FCBCPM * pfcb; char * pfilename;
+{
+    char * pdot, * ptype;
+    int len, nlen, tlen;
+
+    x_memset( pfcb, 0, sizeof( struct FCBCPM ) );
+    x_memset( pfcb->n, ' ', 8 );
+    x_memset( pfcb->t, ' ', 3 );
+
+    len = strlen( pfilename );
+    pdot = x_strchr( pfilename, '.' );
+    if ( pdot )
+    {
+        nlen = (int) ( pdot - pfilename );
+        if ( nlen > 8 )
+            return false;
+        x_memcpy( pfcb->n, pfilename, nlen );
+        ptype = pdot + 1;
+        tlen = strlen( ptype );
+        if ( tlen > 3 )
+            return false;
+        x_memcpy( pfcb->t, ptype, tlen );
+    }
+    else
+    {
+        if ( len > 8 )
+            return false;
+        x_memcpy( pfcb->n, pfilename, len );
+    }
+
+    return true;
+}
+
+uint32_t file_size( pfilename ) char * pfilename;
+{
+    struct FCBCPM size_fcb;
+    uint32_t size = 0;
+    int result;
+
+    fcb_initialize( & size_fcb, pfilename );
+    result = bdos( 35, & size_fcb );
+    if ( 0 == result )
+    {
+#ifdef CPM80
+        /* CP/M 80 doesn't use r2 and it's little-endian */
+        size = ( (uint32_t) size_fcb.r0 + ( ( (uint32_t) size_fcb.r1 ) << 8 ) );
+#else
+        /* CP/M 68K uses r2 and it's big-endian */
+        size = ( (uint32_t) size_fcb.r2 + ( ( (uint32_t) size_fcb.r1 ) << 8 ) + ( ( (uint32_t) size_fcb.r0 ) << 16 ) );
+#endif
+    }
+
+    size <<= 7;   /* 128 bytes per record */
+
+    return size;
 }
 
 char * x_strdup( a ) char * a;
@@ -106,27 +176,33 @@ char ** x_bsearch( key, vbase, num, width, compare ) char ** key; char ** vbase;
       } while ( j >= i );
 
    return 0;
-} //x_bsearch
+} 
 
-int enumerate( fname, ftype ) char * fname; char * ftype;
+int enumerate( pfile ) char * pfile;
 {
-    struct FCBCPM68K * result_fcb;
-    int result, count, i, len;
+#ifdef CPM80
+    char * pappname = "CPMENUMD.COM";
+#else
+    char * pappname = "CPMENUMD.68K";
+#endif
+
+    struct FCBCPM the_fcb;
+    struct FCBCPM * result_fcb;
+    int result, i, len;
     int list_len = 0;
-    char * pthis, * presult;
+    char * pthis, ** presult;
+    uint32_t fsize;
     char file[ 13 ];
     static char * list[ 200 ];
 
-    x_memcpy( the_fcb.n, fname, 8 );
-    x_memcpy( the_fcb.t, ftype, 3 );
+    if ( ! fcb_initialize( & the_fcb, pfile ) )
+        return false;
 
-    result = __BDOS( 17, & the_fcb );
-    count = 0;
+    result = bdos( 17, & the_fcb );
 
     while ( result >= 0 && result <= 3 )
     {
-        count++;
-        result_fcb = (struct FCBCPM68K *) ( & _start - 128 + ( result * 32 ) );
+        result_fcb = (struct FCBCPM *) ( DEFAULT_DMA + ( result * 32 ) );
         len = 0;
 
         for ( i = 0; i < 8; i++ )
@@ -154,55 +230,58 @@ int enumerate( fname, ftype ) char * fname; char * ftype;
         if ( ( sizeof( list ) / sizeof( char * ) ) == list_len )
             break;
         
-        result = __BDOS( 18, & the_fcb );
+        result = bdos( 18, & the_fcb );
+    }
+
+    if ( 255 != result )
+    {
+        printf( "unexpected result from find first: %d\n", result );
+        return false;
     }
 
     qsort( list, list_len, sizeof( char * ), do_compare );
 
-    if ( !strcmp( fname, "????????" ) && !strcmp( ftype, "68K" ) )
-    {
-        char * pkey = "CPMENUMD.68K";
-        char ** presult = x_bsearch( &pkey, list, list_len, sizeof( char * ), do_compare );
-        if ( 0 != presult )
-            printf( "  found this executable! %s\n", *presult );
-    }
-
-    printf( "  result from find first: %d\n", result );
+    presult = x_bsearch( & pappname, list, list_len, sizeof( char * ), do_compare );
+    if ( 0 != presult )
+        printf( "  found this executable! %s\n", *presult );
+    
     for ( i = 0; i < list_len; i++ )
     {
-        printf( "  file %3d: %s\n", i, list[ i ] );
+        fsize = file_size( list[ i ] );
+        printf( "  file %3d: %13s  %8ld\n", i, list[ i ], (long) fsize );
         free( list[ i ] );
     }
 
-    return 0;
+    return true;
 }
 
 int main()
 {
-    x_memset( & the_fcb, 0, sizeof( the_fcb ) );
-
-#if 0 // these will vary too much between systems
+#ifdef CPM80
     printf( "finding all files\n" );
-    enumerate( "????????", "???" );
+    enumerate( "????????.???" );
 
     printf( "finding files that start with A\n" );
-    enumerate( "A???????", "???" );
+    enumerate( "A???????.???" );
 
     printf( "finding C files\n" );
-    enumerate( "????????", "C  " );
+    enumerate( "????????.C" );
 
     printf( "finding PAS files\n" );
-    enumerate( "????????", "PAS" );
+    enumerate( "????????.PAS" );
 #endif
 
+    printf( "finding COM files\n" );
+    enumerate( "????????.COM" );
+
     printf( "finding 68K files\n" );
-    enumerate( "????????", "68K" );
+    enumerate( "????????.68K" );
 
     printf( "finding XYZ files\n" );
-    enumerate( "????????", "XYZ" );
+    enumerate( "????????.XYZ" );
 
     printf( "finding TXT files\n" );
-    enumerate( "????????", "TXT" );
+    enumerate( "????????.TXT" );
 
     printf( "cp/m enumerate directory completed with great success\n" );
     return 0;
