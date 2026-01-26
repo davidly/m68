dbb87270f644de580ede83be2b30a99f0d65ac62
davidly <davidly_lee@hotmail.com>
2026-01-26 12:07:14 -0800

    macos build fix

--- aztec68k/tests/msymto68k.sh
+++ aztec68k/tests/msymto68k.sh
@@ -1,1 +1,5 @@
-g++ -O3 -fno-builtin -I . -I ../.. symto68k.cxx -o symto68k -static
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    g++ -O2 -I . -I ../.. symto68k.cxx -o symto68k
+else
+    g++ -O3 -fno-builtin -I . -I ../.. symto68k.cxx -o symto68k -static
+fi
